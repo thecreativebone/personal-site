771a6ebe007f142fac518dcd9b39fc9ab461d87b
thecreativebone <ryan@thecreativebone.com>
2016-03-21 09:35:58 -0700

    complete revamp

--- contact.php
+++ contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "example@themeforest.com";
+$address = "mailto:ryan@thecreativebone.com";
 
 
 // Configuration option.
